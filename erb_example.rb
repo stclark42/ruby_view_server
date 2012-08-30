b56ab7c692746cf4de5e486ec25525c92f984902
Stephen Clark <stclark42@gmail.com>
2012-08-30 16:23:26 -0500

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,18 @@
 require 'erb'
 
-x = 42
+x = 49
+nums = Array[5, 4, 3, 2, 1]
+months = Hash.new('month')
+months = {"1" => "January", "2" => "February", "3" => "March", "4" => "April", "5" => "May", "6" => "June", "7" => "July", "8" => "August", "9" => "September", "10" => "October", "11" => "November", "12" => "December" }
+word = String.new("Bird")
+
 template = ERB.new "The value of x is: <%= x %>"
+numtest = ERB.new "The 5th number is <%= nums[2] %>"
+monthtest = ERB.new "The 4th month is <%= months['4'] %>"
+stringtest = ERB.new "<%= word %> is the word."
+
 puts template.result(binding)
+puts numtest.result(binding)
+puts monthtest.result(binding)
+puts stringtest.result(binding)
+
